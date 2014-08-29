db4159231224e29e1657e787af08341a1948a97b
Felipe Pierin <felipe.pierin@gmail.com>
2014-08-29 19:27:18 -0300

    inclusao de arquivo

---
+++ ep01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# Author: Felipe Lombardi Pierin
+# Nusp:	5875242
+
+#             hello
+#       +---------------+
+#       1               1
+#   +-------+       +-------+
+#   2       3       2       3
+# +---+   +---+   +---+   +---+
+# 4   5       6   4   5       6
+
+
+def main():
+	A = [18,5,100,3,1,19,6,0,7,4,2]
+	print A
+
+if __name__ == "__main__":
+	main()
